00cad69635b1cd6e1a1a5d8c5d355399d4462d4c
Korolov-Artem <justanotheremaildx@gmail.com>
2024-09-14 22:24:40 -0600

    refactor Users to class component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,34 +3,33 @@ import "./Users.css";
 import User from "./User/User";
 import axios from "axios";
 
-const Users = (props) => {
-  let getUsers = () => {
-    if (props.state.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((users) => {
-          props.setUsers(users.data.items);
-        });
-    }
-  };
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
 
-  let UsersElements = props.state.map((user) => (
-    <User
-      key={user.id}
-      follow={props.follow}
-      unfollow={props.unfollow}
-      state={user}
-    />
-  ));
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((users) => {
+        this.props.setUsers(users.data.items);
+      });
+  }
 
-  return (
-    <div className="Users">
-      <button className="Users__getUsers" onClick={getUsers}>
-        Get Users
-      </button>
-      <div className="UsersPage__users">{UsersElements}</div>
-    </div>
-  );
-};
+  render() {
+    let UsersElements = this.props.state.map((user) => (
+      <User
+        key={user.id}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+        state={user}
+      />
+    ));
+
+    return (
+      <div className="Users">
+        <div className="UsersPage__users">{UsersElements}</div>
+      </div>
+    );
+  }
+}
 
 export default Users;
